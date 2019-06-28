09d883f51a1263a822b7b04951669f27f5b11e1d
elipmoc <relipmoc101@gmail.com>
2019-06-28 14:10:07 +0900

    test

--- game/src/main.ts
+++ game/src/main.ts
@@ -1,7 +1,8 @@
 import { PlayScene } from "./scenes/PlayScene";
+import { EndScene } from "./scenes/EndScene";
 
 function main(param: g.GameMainParameterObject): void {
-	g.game.pushScene(PlayScene());
+	g.game.pushScene(EndScene());
 }
 
 export =main;
